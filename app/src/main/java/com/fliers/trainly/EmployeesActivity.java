b4ef00b2303548b19048394a56ce8dee5d1ff80d
alpafyonluoglu <alpafyonluoglu@gmail.com>
2021-04-23 22:38:15 +0300

    Added all activities and XML files

---
+++ app/src/main/java/com/fliers/trainly/EmployeesActivity.java
@@ -0,0 +1,14 @@
+package com.fliers.trainly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employees);
+    }
+}
