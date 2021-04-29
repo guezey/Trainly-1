e89254e0028375ab331be82b00ed4f88a7e5262a
erkin <erkinresearch@gmail.com>
2021-04-29 10:49:54 +0300

    Updates on AddTrainActivity and TrainsActivity

--- app/src/main/java/com/fliers/trainly/activities/AddTrainActivity.java
+++ app/src/main/java/com/fliers/trainly/activities/AddTrainActivity.java
@@ -2,15 +2,29 @@ package com.fliers.trainly.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.fliers.trainly.R;
 
+import java.awt.Button;
+
 public class AddTrainActivity extends AppCompatActivity {
+
+    private Spinner placeSpinner;
+    Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_train);
+
+        placeSpinner = (Spinner) findViewById(R.id.lineSpinner);
+        add = findViewById(R.id.buttonAdd);
     }
+
+    add.setOnClickListener( new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            switchToTrainDetails();
+        }
+    });
 }
