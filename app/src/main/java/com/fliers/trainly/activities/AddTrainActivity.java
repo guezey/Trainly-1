1d353ac9bd1622da8fbf850a37b48af0748cfb4a
erkin <erkinresearch@gmail.com>
2021-04-29 11:49:34 +0300

    Updates on AddTrainActivity

--- app/src/main/java/com/fliers/trainly/activities/AddTrainActivity.java
+++ app/src/main/java/com/fliers/trainly/activities/AddTrainActivity.java
@@ -6,25 +6,59 @@ import com.fliers.trainly.R;
 
 import java.awt.Button;
 
+import javax.swing.text.View;
+
 public class AddTrainActivity extends AppCompatActivity {
 
-    private Spinner placeSpinner;
-    Button add;
+    private Spinner departurePlaceSpinner;
+    private Spinner arrivalPlaceSpinner;
+    private TextView txtBusinessWagonNum;
+    private TextView txtEconomyWagonNum;
+    Button goDetails;
+    Button addTrain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_train);
 
-        placeSpinner = (Spinner) findViewById(R.id.lineSpinner);
-        add = findViewById(R.id.buttonAdd);
+        departurePlaceSpinner = (Spinner) findViewById(R.id.departurePlaceSpinner);
+        arrivalPlaceSpinner = (Spinner) findViewById(R.id.arrivalPlaceSpinner);
+        txtBusinessWagonNum = (TextView) findViewById(R.id.textView30);
+        txtEconomyWagonNum = (TextView) findViewById(R.id.textView31);
+        goDetails = findViewById(R.id.buttonGoDetails);
     }
 
-    add.setOnClickListener( new View.OnClickListener() {
+    goDetails.setOnClickListener( new View.OnClickListener() {
 
-        @Override
         public void onClick(View view) {
             switchToTrainDetails();
         }
     });
+
+    addTrain.setOnClickListener( new View.OnClickListener() {
+
+        public void onClick(View view) {
+            goToManageTrains();
+        }
+    });
+
+    public void goToManageTrains() {
+
+
+
+
+
+        Intent switchActivityIntent = new Intent(this, TrainsActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
+    public void switchToTrainDetails() {
+
+        txtBusinessWagonNum = (TextView) findViewById(R.id.textView30);
+        txtEconomyWagonNum = (TextView) findViewById(R.id.textView31);
+        txtBusinessWagonNum.setText("0");
+        txtEconomyWagonNum.setText("0");
+        addTrain = findViewById(R.id.buttonAddTrain);
+    }
 }
