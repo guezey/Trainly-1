ffd0ee7000afec736eca5cecc495d25bd8090f18
erkin <erkinresearch@gmail.com>
2021-05-01 14:36:34 +0300

    Updates on LinesActivity

--- app/src/main/java/com/fliers/trainly/activities/LinesActivity.java
+++ app/src/main/java/com/fliers/trainly/activities/LinesActivity.java
@@ -12,29 +12,51 @@ import javax.swing.text.View;
 
 public class LinesActivity extends AppCompatActivity {
 
-    private TextView place1;
-    private TextView place2;
+    private TextView textView20;
+    private TextView textView21;
+    private EditText editTextTextPersonName2;
+    private EditText editTextTextPersonName3;
     private Textview x1;
     private TextView y1;
     private TextView x2;
     private TextView y2;
-    private Button saveButton;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lines);
 
-        place1 = (TextView) findByViewId(R.id.textPlace1);
-        place2 = (TextView) findbyViewId(R.id.textPlace2);
-        saveButton = findViewById(R.id.buttonSaveButton);
+        textView20 = (TextView) findByViewId(R.id.textView20);
+        textView21 = (TextView) findbyViewId(R.id.textView21);
+        editTextTextPersonName2 = (EditText) findViewById(R.id.editTextTextPersonName2);
+        editTextTextPersonName3 = (EditText) findViewById(R.id.editTextTextPersonName3);
+        button = findViewById(R.id.button);
     }
 
-    saveButton.setOnClickListener( new View.OnClickListener() {
+    button.setOnClickListener( new View.OnClickListener() {
 
         public void onClick(View view) {
 
-            if(place)
+            if( editTextTextPersonName2.equals("") || editTextTextPersonName2.equals("Place 1") || editTextTextPersonName3.equals("") || editTextTextPersonName3.equals("Place 2")) {
+
+                editTextTextPersonName2.setError("Invalid Input");
+                editTextTextPersonName3.setError("Invalid Input");
+            }
+            else if(editTextTextPersonName2.equals("") || editTextTextPersonName2.equals("Place 1")) {
+
+                editTextTextPersonName2.setError("Invalid Input");
+            }
+            else if( editTextTextPersonName3.equals("") || editTextTextPersonName3.equals("Place 2")) {
+
+                editTextTextPersonName3.setError("Invalid Input");
+            }
+
+            else{
+
+                //TO-DO
+            }
         }
     });
+
 }
