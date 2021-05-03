827c1d84cf463c730311732ec88de8b0e99e9ec4
erkin <erkinresearch@gmail.com>
2021-05-03 17:34:17 +0300

    Updates on many things...

--- app/src/main/java/com/fliers/trainly/activities/EmployeesActivity.java
+++ app/src/main/java/com/fliers/trainly/activities/EmployeesActivity.java
@@ -2,15 +2,41 @@ package com.fliers.trainly.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.fliers.trainly.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EmployeesActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private FloatingActionButton addEmployee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees);
+
+        back = (ImageView) findViewById(R.id.imageView2);
+        addEmployee = (FloatingActionButton) findViewById(R.id.fabAdd);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getApplicationContext(), AddEmployeeActivity.class);
+                startActivity( intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/fliers/trainly/activities/MainActivity.java
+++ app/src/main/java/com/fliers/trainly/activities/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent( getApplicationContext(), TrainsActivity.class);
+        Intent intent = new Intent( getApplicationContext(), EmployeesActivity.class);
         startActivity( intent);
     }
 }
